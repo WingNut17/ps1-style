aa00ded49a0feb9e8a055936d7ad106785963a3b
Shawn <shawn.sinclair56@proton.me>
2025-07-24 20:49:03 -0400

    init

---
+++ main/player/player.gd
@@ -0,0 +1,89 @@
+extends CharacterBody3D
+
+
+@export var head : Node3D
+
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const CROUCH_SPEED = 4.0  
+
+var can_move: bool = true:
+	set(value):
+		can_move = value
+		head.can_move = value
+
+var input_direction: Vector2 = Vector2.ZERO
+var shape: Shape3D
+var crouching: bool = false
+var target_height: float
+var normal_height: float = 2.0
+var crouch_height: float = 1.25
+var target_collision_height: float
+var normal_collision_height: float = 0.0
+var crouch_collision_height: float = 0.375
+
+
+func _ready() -> void:
+	shape = collision_shape.shape
+	normal_height = shape.size.y  # Use size instead of extents for newer Godot versions
+	target_height = normal_height
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("crouch"):
+		crouch()
+	if event.is_action_released("crouch"):
+		crouch()
+	if event.is_action_pressed("jump") and is_on_floor() and not crouching:
+		jump()
+	
+	input_direction = Input.get_vector("left", "right", "forward", "backward")
+
+func _physics_process(delta: float) -> void:
+	if !can_move:
+		return
+
+	# Smoothly interpolate the collision shape height
+	var current_height = shape.size.y
+	var current_collision_height = collision_shape.position.y
+	if abs(current_height - target_height) > 0.01:  # Only update if there's a meaningful difference
+		shape.size.y = lerpf(current_height, target_height, CROUCH_SPEED * delta)
+		collision_shape.position.y = lerpf(current_collision_height, target_collision_height, CROUCH_SPEED * delta)
+	
+	# Add the gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Get the input direction and handle the movement/deceleration
+	var horizontal_basis = Basis(Vector3.UP, head.rotation.y)
+	var direction := (horizontal_basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	
+	var current_speed = SPEED
+	if crouching:
+		current_speed *= 0.5  # Slower movement when crouching
+	
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+	
+	move_and_slide()
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+
+func crouch():
+	if not crouching:
+		crouching = true
+		target_height = crouch_height
+		target_collision_height = crouch_collision_height
+	elif crouching:
+		crouching = false
+		target_height = normal_height
+		target_collision_height = normal_collision_height
+
+func interact_ended():
+	can_move = !can_move
---
+++ scripts/systems/interaction/interact_actions.gd
@@ -0,0 +1,7 @@
+class_name InteractActions
+extends Resource
+
+@export var cutscene: Animation
+@export var dialogue: DialogueResource
+@export var item: Item
+@export_enum("door", "lever", "other") var button: String
