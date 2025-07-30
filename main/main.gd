9ad3d6dc5c31046d203e20078fd2d0c038fefcd8
Shawn <shawn.sinclair56@proton.me>
2025-07-30 02:19:00 -0400

    added assets and leveltransition effects

--- main/main.gd
+++ main/main.gd
@@ -3,12 +3,12 @@ extends Node3D
 @onready var world: Node = $World
 @onready var player: Node3D = $Player
 
-"""
+
 func _ready() -> void:
 	# Start the game on the first level
 	if world.has_method("switch_to_level"):
 		world.switch_to_level("level_1")
-"""
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("dev_quit"):
